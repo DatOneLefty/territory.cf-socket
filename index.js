3cccec1b5f93204719978c74382cc10a0fb79a04
DatOneLefty <k9dogs101@gmail.com>
2017-04-14 14:26:50 -0400

    save map on movement

--- index.js
+++ index.js
@@ -4,9 +4,8 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var express = require("express");
 var mkdirp = require('mkdirp');
-
-
-
+var map_data = JSON.parse(fs.readFileSync(__dirname + "/data/map", "utf8"));
+var port = 25500; // port for server to run on
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/static/index.html');
 });
@@ -16,9 +15,6 @@ io.on('connection', function(socket){
 
 });
 
-http.listen(25500, function(){
-  console.log('listening on *:25500');
-});
 
 
 io.on('connection', function(socket){
@@ -31,10 +27,13 @@ io.on('connection', function(socket){
 
 io.on('connection', function(socket){
   socket.on('move', function(msg){
-    var coords;
-    console.log(msg);
-    var ret = msg.split(",");
-    io.emit('new-player', msg);
+    var claim = msg.split(",");
+    console.log("CLAIM:" + claim[0] + "," + claim[1] + " by " + claim[2]);
+    var tdir = __dirname + "/data/" + claim[2] + "/";
+    fs.writeFile(tdir + "x", claim[0]);
+    fs.writeFile(tdir + "y", claim[1]);
+    io.emit('new-claim', claim[0] + "," + claim[1] + ',' + fs.readFileSync(tdir + "color", "utf8"));
+    map_data = map_data.concat([claim]);
   });
 });
 
@@ -44,10 +43,9 @@ io.on('connection', function(socket){
 
 	var tdir = __dirname + "/data/" + message + "/";
 	var x,y,c;
-	x = file_get_cont(tdir + "x");
-	y = file_get_cont(tdir + "y");
-  c = file_get_cont(tdir + "color");
-
+  x= fs.readFileSync(tdir + "x", "utf8");
+  y= fs.readFileSync(tdir + "y", "utf8");
+  c= fs.readFileSync(tdir + "color", "utf8");
 
   	console.log("telling user " + message + ": " +  x + "," + y + "," + c);
   	this.emit("info", x + "," + y + "," + c);
@@ -78,11 +76,23 @@ io.on('connection', function(socket){
 	mkdirp(__dirname + "/data/" + ms, function(err) {
 	});
 	var tdir = __dirname + "/data/" + ms + "/";
-	fs.writeFile(tdir + "color",'#'+(Math.random()*0xFFFFFF<<0).toString(16));
+	fs.writeFile(tdir + "color", '#'+(Math.random()*0xFFFFFF<<0).toString(16));
 	fs.writeFile(tdir + "x", Math.round(getRandomInt(-1000,1000)));
 	fs.writeFile(tdir + "y", Math.round(getRandomInt(-1000,1000)));
 	fs.writeFile(tdir + "claimed", "0");
 	this.emit("new-info", ms);
 	console.log("user join (NEW): " + ms);
 	});
+});
+
+
+setInterval(function() {
+fs.writeFile(__dirname + "/data/map", JSON.stringify(map_data));
+console.log("SERVER: map backed up");
+}, 10000);
+
+
+
+http.listen(port, function(){
+  console.log('server initialized. listening on port ' + port);
 });
