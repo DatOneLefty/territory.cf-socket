908f08e5ac3fd22bf12ab57d4ca8147cbdf0d50a
captain <k9dogs101@gmail.com>
2017-04-13 20:45:36 -0400

    more stuff

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.get('/', function(req, res){
 
 app.use('/external/', express.static(__dirname + "/static/"));
 io.on('connection', function(socket){
-  console.log('a user connected');
+  
 });
 
 http.listen(3000, function(){
@@ -22,9 +22,9 @@ http.listen(3000, function(){
     
 
 io.on('connection', function(socket){
-  console.log('a user connected');
+ 
   socket.on('disconnect', function(){
-    console.log('user disconnected');
+
   });
 });
     
@@ -40,17 +40,42 @@ io.on('connection', function(socket){
 
 io.on('connection', function(socket){
   socket.on('new-join', function(message) {
-	this.emit("info", "0,0,0e0e0e");
+	console.log("user join: " + message);
+
+
+	console.log("telling user " + message + ": " +  x + "," + y + "," + c);
+	this.emit("info", x + "," + y + "," + c);
+
+	var tdir = __dirname + "/data/" + message + "/";
+	var x,y,c;
+	fs.readFile(tdir + "x", 'utf8', function (err,data) {
+		x = data;
+		console.log(x);
+	});
+	fs.readFile(tdir + "y", 'utf8', function (err,data) {
+		y = data;
+	});
+	fs.readFile(tdir + "color", 'utf8', function (err,data) {
+		c = data;
+	});
 	});
 });
+function getRandomInt(min, max) {
+	return Math.floor(Math.random() * (max-min + 1)) + min;
+}
 
 io.on('connection', function(socket){
   socket.on('new-user', function(message) {
 	var c = new Date();
 	var ms = c.getTime();
-	mkdirp(__dirname + "/" + ms, function(err) { 
+	mkdirp(__dirname + "/data/" + ms, function(err) { 
 	});
+	var tdir = __dirname + "/data/" + ms + "/";
+	fs.writeFile(tdir + "color",'#'+(Math.random()*0xFFFFFF<<0).toString(16));
+	fs.writeFile(tdir + "x", Math.round(getRandomInt(-1000,1000)));
+	fs.writeFile(tdir + "y", Math.round(getRandomInt(-1000,1000)));
+	fs.writeFile(tdir + "claimed", "0");
 	this.emit("new-info", ms);
+	console.log("user join (NEW): " + ms);
 	});
 });
-
